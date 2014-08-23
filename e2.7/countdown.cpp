dc08936ad7ae0625dc44729ef568aab36a829c19
Giorgos <admin@gkomninos.com>
2014-08-23 10:17:41 +0200

    added exercise 2.7

---
+++ e2.7/countdown.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using std::cout;        using std::endl;
+
+int main()
+{
+        for(int i=10; i>=-5;--i){
+                cout << i << endl;
+        }
+        return 0;
+}
