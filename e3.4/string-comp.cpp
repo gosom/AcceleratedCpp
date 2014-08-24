8ba266bc90c1049bcc726077af3bd1c5c9c32c69
Giorgos <admin@gkomninos.com>
2014-08-24 10:48:02 +0200

    exercise 3.4

---
+++ e3.4/string-comp.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using std::cout;    using std::cin;
+using std::endl;    using std::string;
+
+int main()
+{
+    string in = "";
+    string::size_type min = in.max_size();
+    string::size_type max = 0;
+
+    cout << endl;
+    cout << "Please give some words: ";
+    while (cin >> in){
+        string::size_type cs = in.size();
+        if (cs >= max)
+            max = cs;
+        if (cs <= min)
+            min = cs;
+    }
+    cout << endl;
+    cout << "Longest: " << max << " Shortest: " << min << endl;
+    return 0;
+}
